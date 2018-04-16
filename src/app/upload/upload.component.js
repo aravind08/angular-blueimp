9c28070a063c37647788a4960e79ce4f43e547b4
aravind_cf <aravind@capitalfloat.com>
2018-04-16 11:18:36 +0530

    added responsive video; improved playback login

--- src/app/upload/upload.component.js
+++ src/app/upload/upload.component.js
@@ -19,7 +19,7 @@
     var date = new Date();
 
     vm.file_name = '';
-    vm.uploader_message = "supported formats: avi | wmv | flv | mpg | mp4";
+    showMessage("supported formats: avi | wmv | flv | mpg | mp4");
     vm.progress = {
       width: 0 + '%'
     };
@@ -53,11 +53,18 @@
       },
       done: function (e, data) {
         // call the media api to fetch the video
-        getPlaybackMedia(data);
+        vm.show_loader = true;
+        vm.show_video = true;
+        showMessage("Processing the video. Please wait... ");
+        if(data.result.hashed_id)
+          getPlaybackMedia(data.result.hashed_id);
+        else 
+          showMessage("Error getting the video for playback!"); 
       },
       fail: function (e, data) {
         // clear the videoes if it's 403 and retry uploading
         if (data.jqXHR.status == 403) {
+          showMessage("The account has reached the video upload limit. Please wait while we clear the videoes...");
           clearVideosForUpload(data);
         }
       },
@@ -69,61 +76,49 @@
 
 
     function clearVideosForUpload(data) {
-      $timeout(function () {
-        vm.uploader_message = "The account has reached the video upload limit. Please wait while we clear the videoes...";
-      }, 0);
-      var config = {
-        params: {
-          'api_password': CONSTANTS.WISTIA.password,
-          'project_id': CONSTANTS.WISTIA.projectId
-        }
-      };
-      $http.get(CONSTANTS.WISTIA.mediaUrl, config).then(function (res) {
-        console.log(res);
-        for (var i = 0; i < res.data.length; i++){
-          deleteMedia(res.data[i].id);
-          if (i < 2) {
-            console.log("data.submit");
-            console.log(data);
-            data.submit();
+
+      // Delete the selected media from Wistia bucket
+      function deleteMedia(id, i) {
+        var config = {
+          params: {
+            'api_password': CONSTANTS.WISTIA.password,
           }
-        }
-      }, function (err) {
-        $timeout(function () {
-          console.log("Oops again! Error while fetching the media list.");
-        }, 0);
-      });
-    }
+        };
+        $http.delete(CONSTANTS.WISTIA.mediaActionUrl+id+'.json', config).then(function (res) {
+          // start uploading the video once the video count goes below 3
+          if (i == 0)
+            data.submit();
+        }, function (err) {
+            showMessage("Oops again! Error while deleting the media");
+        });
+      }
 
-
-    function deleteMedia(id) {
-      var config = {
-        params: {
-          'api_password': CONSTANTS.WISTIA.password,
-        }
-      };
-      $http.delete(CONSTANTS.WISTIA.mediaActionUrl+id+'.json', config).then(function (res) {
-        // do nothing
-      }, function (err) {
-        $timeout(function () {
-          vm.uploader_message = "Oops again! Error while deleting the media";
-        }, 0);
-      });
+      return (function () {
+        var config = {
+          params: {
+            'api_password': CONSTANTS.WISTIA.password,
+            'project_id': CONSTANTS.WISTIA.projectId
+          }
+        };
+        $http.get(CONSTANTS.WISTIA.mediaUrl, config).then(function (res) {
+          for (var i = 0; i < res.data.length; i++) {
+            deleteMedia(res.data[i].id, i);
+          }
+        }, function (err) {
+            showMessage("Oops again! Error while fetching the media list.");
+        });
+      }());
     }
 
 
     function checkFileTypes(data) {
       var accepted_file_types = /^video\/\.*(avi|AVI|wmv|WMV|flv|FLV|mpg|MPG|mp4|MP4)$/;
       if (data.originalFiles.length > 1) {
-        $timeout(function () {
-          vm.uploader_message = "Upload failed! Please upload one video at a time";
-        }, 0);
+          showMessage("Upload failed! Please upload one video at a time");
         return;
       }
       if (data.originalFiles[0]['type'].length && !accepted_file_types.test(data.originalFiles[0]['type'])) {
-        $timeout(function () {
-          vm.uploader_message = "Upload failed! Unsupported file type.";
-        }, 0);
+          showMessage("Upload failed! Unsupported file type.");
         return;
       }
       
@@ -134,7 +129,7 @@
 
     function uploadProgressAll(data) {
       vm.show_loader = true;
-      vm.uploader_message = "Video is being uploaded. Please wait...";
+      showMessage("Video is being uploaded. Please wait...");
       var progress = parseInt(data.loaded / data.total * 100, 10);
       $timeout(function () {
         vm.progress = {
@@ -143,60 +138,48 @@
       }, 0);
     }
 
+    function showMessage(msg) {
+      $timeout(function () { 
+        vm.uploader_message = msg;
+      }, 0);
+    }
 
-    function getPlaybackMedia(data) {
 
-      function success(res) {
-        // check progress
-        var progress = parseInt(res.data[0].progress * 100, 10);
-        var image_html = '<img src="' + res.data[0].thumbnail.url + '" style="width: 100%; height: 360px; background-color: #000">';
-        var iframe_html = '<iframe src="' + CONSTANTS.WISTIA.embedUrl + id + '" allowfullscreen mozallowfullscreen webkitallowfullscreen oallowfullscreen msallowfullscreen allowtransparency="true" frameborder="0" scrolling="no" class="wistia_embed" name="wistia_embed" style="width: 100%; height: 360px"></iframe>';
-  
+    function getPlaybackMedia(hashed_id) {
+      
+      var params = {
+        api_password: CONSTANTS.WISTIA.password,
+        hashed_id: hashed_id
+      };
+      
+      $http.get(CONSTANTS.WISTIA.mediaUrl, { params: params }).then(function(res) {
+        var iframe_html = '<iframe src="' + CONSTANTS.WISTIA.embedUrl + hashed_id + '" allowfullscreen mozallowfullscreen webkitallowfullscreen oallowfullscreen msallowfullscreen allowtransparency="true" frameborder="0" scrolling="no" class="embed-responsive-item" name="wistia_embed"></iframe>';
         
-        // retry if not completed
-        if (progress < 100) {
-  
+        // update progress
+        $timeout(function(){
+          vm.progress.width = parseInt(res.data[0].progress * 100, 10) + '%';
+          vm.video_preview_html = $sce.trustAsHtml(iframe_html);
+        }, 0);
+
+        // retry every 2 seconds if the status is still queued
+        if (res.data[0].status == "queued") {  
           $timeout(function () {
-            vm.show_video = true;
-            vm.video_name = res.data[0].name;
-            vm.progress.width = progress+'%';            
-            vm.video_preview_html = $sce.trustAsHtml(image_html);
-          }, 0);
-  
-          // retry until video processing completes
-          $timeout(function () {
-            $http.get(CONSTANTS.WISTIA.mediaUrl, { params: params }).then(success, error);
-          }, 1000 * 5);
+            getPlaybackMedia(hashed_id);
+          }, 2000);
 
           return;
         }
   
-        $timeout(function () {
-          vm.video_preview_html = $sce.trustAsHtml(iframe_html);
-          vm.uploader_message = "Ready!";
-          vm.show_loader = false;
-        }, 0);
-      }
-  
-
-      function error(err) {
-        $timeout(function () {
+        showMessage("Ready!");
+        showMessage(res.data[0].name);
+        
+        vm.show_loader = false;
+      }, function (err) {
           vm.show_loader = false;
           vm.show_video = false;
-          vm.uploader_message = "Oops! Something went wrong!";
-        }, 0);
-      }
-
-      vm.show_loader = true;
-      vm.uploader_message = "Processing the video. Please wait... ";
-      var id = data.result.hashed_id;
-      var params = {
-        api_password: CONSTANTS.WISTIA.password,
-        hashed_id: id
-      };
-      $http.get(CONSTANTS.WISTIA.mediaUrl, { params: params }).then(success, error);
+          showMessage("Oops! Something went wrong!");
+      });
     }
-
   }
 
 })(window.angular, window.jQuery);
